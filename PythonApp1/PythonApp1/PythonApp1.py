d219b7d48e8beca982ecfdb1cdd13c4d96af2aff
Lekuro <lekurot@gmail.com>
2020-11-11 23:33:05 +0200

    have learned if, list, for in, while, range, boolean

--- PythonApp1/PythonApp1/PythonApp1.py
+++ PythonApp1/PythonApp1/PythonApp1.py
@@ -76,3 +76,200 @@ y = 'ok'
 print(y)
 x += y
 print(x)
+
+x = True
+print(x)
+print(2 == 3)
+print("hello" == "hello")
+print( 1 != 1 )
+print("eleven" != "seven")
+print(2 != 10)
+print( 7 > 5 )
+print( 10 < 10.0 )
+print(7 <= 8)
+print(9 >= 9.0)
+print("Annie" > "Andy")
+
+if 10 > 5 :
+    print('10 is greate than 5')
+num = 12
+if num > 5:
+   print("Bigger than 5")
+   if num <=47:
+      print("Between 5 and 47")
+x = 4
+if x == 5:
+   print("Yes")
+else:
+   print("No")
+num = 3
+if num == 1:
+  print("One")
+else: 
+  if num == 2:
+    print("Two")
+  else: 
+    if num == 3: 
+      print("Three")
+    else: 
+      print("Something else")
+
+print( 1 == 1 and 2 == 2 )
+print( 1 == 1 and 2 == 3 )
+print( 1 != 1 and 2 == 2 )
+print( 2 < 1 and 3 >  6 )
+
+print( 1 == 1 or 2 == 2 )
+print( 1 == 1 or 2 == 3 )
+print( 1 != 1 or 2 == 2 )
+print( 2 < 1 or 3 >  6 )
+
+print(not 1 == 1)
+print(not 1 > 7)
+
+print( False == False or True )
+print( False == (False or True) )
+print( (False == False) or True )
+
+grade = 88
+if (grade >= 70 and grade <=100):
+    print("Passed!") 
+
+x = 4
+y = 2
+if not 1 + 1 == y or x == 4 and 7 == 8:
+  print("Yes")
+elif x > y:
+  print("No")
+
+words = ["Hello", "world", "!"]
+print(words[0])
+print(words[1])
+print(words[2]) 
+
+empty_list = []
+print(empty_list)
+number = 3
+things = ["string", 0, [1, 2, number], 4.56]
+print(things[1])
+print(things[2])
+print(things[2][2])
+m = [
+    [1, 2, 3],
+    [4, 5, 6]
+    ]    
+print(m[1][2])  
+
+string = "Hello world!"
+print(string[6])
+
+nums = [7, 7, 7, 7, 7]
+nums[2] = 5
+print(nums)
+
+nums = [1, 2, 3]
+print(nums + [4, 5, 6])
+print(nums * 3)
+
+words = ["spam", "egg", "spam", "sausage"]
+print("spam" in words)
+print("egg" in words)
+print("tomato" in words)
+
+nums = [1, 2, 3]
+print(not 4 in nums)
+print(4 not in nums)
+print(not 3 in nums)
+print(3 not in nums)
+
+nums = [1, 2, 3]
+nums.append(4)
+print(nums)
+
+nums = [1, 3, 5, 2, 4]
+print(len(nums))
+
+words = ["Python", "fun"]
+index = 1
+words.insert(index, "is")
+print(words)
+
+letters = ['p', 'q', 'r', 's', 'p', 'u']
+print(letters)
+print(letters.index('r'))
+print(letters.index('p'))
+print(letters.count('p'))
+print(len(letters))
+print(max(letters))
+print(min(letters))
+letters.remove('p')
+print(letters)
+letters.reverse()
+print(letters)
+
+i = 1
+while i <=5:
+   print(i)
+   i = i + 1
+
+print("Finished!")
+
+x = 1
+while x < 10:
+  if x%2 == 0:
+    print(str(x) + " is even")
+  else:
+    print(str(x) + " is odd")
+
+  x += 1 
+
+  i = 0
+while True:
+  print(i)
+  i = i + 1
+  if i >= 5:
+    print("Breaking")
+    break
+
+print("Finished")
+
+i = 1
+while i<=5:
+    print(i)
+    i+=1
+    if i==3:
+      print("Skipping 3")
+      continue
+
+words = ["hello", "world", "spam", "eggs"]
+for word in words:
+  print(word + "!")
+
+string = "testing for loops"
+count = 0
+
+for x in string:
+  if(x == 't'):
+    count += 1
+
+print(count) 
+
+numbers = list(range(10))
+print(numbers)
+
+numbers = list(range(3, 8))
+print(numbers)
+
+print(range(20) == range(0, 20))
+
+numbers = list(range(5, 20, 2))
+print(numbers)
+
+numbers = list(range(20, 5, -2))
+print(numbers)
+
+for i in range(5):
+  print("hello!")
+
+list = [1, 1, 2, 3, 5, 8, 13]
+print(list[list[4]])
