ed3cbfe035033b9a180d07317d7cdf5ba734a11e
Lekuro <lekurot@gmail.com>
2020-11-11 20:29:33 +0200

    have learned string and variable

--- PythonApp1/PythonApp1/PythonApp1.py
+++ PythonApp1/PythonApp1/PythonApp1.py
@@ -4,6 +4,7 @@ print(2 + 3)
 print(2 - 8)
 print(2*3+3*(8-5))
 print(10/2)
+print( 0.42 )
 print( 6 * 7.0 )
 print( 4 + 1.65 )
 print((2**3)**2)
@@ -14,3 +15,64 @@ print(20//6)
 print(20%6)
 print(8.3//4.1)
 print(1.25%0.5)
+
+print("Pyton is fun")
+print('Always look on the bright side of life')
+print('Bob\'s mother: my son isn\'t an angel.')
+print('Bob"s mother: my son isn"t an angel.')
+print("Bob's mother: my son isn't an angel.")
+print('One \nTwo \nThree') 
+print("""This
+is a
+multiline
+text""") 
+
+print('''This is 
+multiline 
+text
+too''')
+print('Some' + ' text')
+print('2' + '2')
+print('Some text * 3 ' * 3)
+print(2 * '2 * text usually stay first ')
+
+this_is_a_normal_name_of_variable = 'I am a value of this variable'
+print(this_is_a_normal_name_of_variable)
+print('Python is a case sensitive programming language. Thus, Lastname and lastname are two different variable names in Python.')
+x = 7
+print(x)
+print(x + 3)
+print(x)
+x = 123.456
+print(x)
+x = "This is a string"
+print(x + "!")
+print('However, it is not good practice. To avoid mistakes, try to avoid overwriting the same variable with different data types.')
+x = input('Enter something please: ')
+print('You enter ' + x)
+x = int(input('please enter your age: '))
+print('Are you ' + str(x) + ' years old?')
+x = 8
+y = 3
+x += y
+print(x)
+x -= y
+print(x)
+x *= y
+print(x)
+x /= y
+print(x)
+x %= y
+print(x)
+x **= y
+print(x)
+x //= y
+print(x)
+x = 'i am string '
+print(x)
+x *= 3
+print(x)
+y = 'ok'
+print(y)
+x += y
+print(x)
